a31649154322e159373c3065a1834b7a6e7b847b
Fernando Nogueira Risso <48661787+ferisso@users.noreply.github.com>
2022-01-23 10:46:28 -0300

    ensure is auth

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { ensureAdmin } from './middlewares/ensureAdmin';
+
+// controllers
 import { CreateUserController } from "./controllers/CreateUsersController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { AuthenticateJwtController } from "./controllers/AuthenticateJwtController";
 import { CreateCompliementController } from "./controllers/CreateCompliementController";
+
+// middlewares
+import { isAuth } from "./middlewares/ensureAuthenticate";
+import { ensureAdmin } from './middlewares/ensureAdmin';
 
 const router = Router();
 
@@ -11,7 +16,7 @@ const createUserController = new CreateUserController()
 router.post("/users", createUserController.handle)
 
 const createTagController = new CreateTagController()
-router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/tags", isAuth, ensureAdmin, createTagController.handle)
 
 const authenticateJwtController = new AuthenticateJwtController()
 router.post("/authenticate", authenticateJwtController.handle)
