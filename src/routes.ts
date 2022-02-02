14528c3d047c58f1d748b49cce6a6f65072999da
fernando risso <f.risso6@hotmail.com>
2022-02-02 19:34:41 -0300

    listing users

--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,17 @@ import { CreateUserController } from "./controllers/CreateUsersController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { AuthenticateJwtController } from "./controllers/AuthenticateJwtController";
 import { CreateCompliementController } from "./controllers/CreateCompliementController";
+import { ListUserController } from "./controllers/ListUserController"
 
 // middlewares
 import { isAuth } from "./middlewares/ensureAuthenticate";
 import { ensureAdmin } from './middlewares/ensureAdmin';
 
 const router = Router();
-
+// Get
+const listUserController = new ListUserController()
+router.get("/users", isAuth, listUserController.handle)
+// Posts
 const createUserController = new CreateUserController()
 router.post("/users", createUserController.handle)
 
@@ -23,5 +27,7 @@ router.post("/authenticate", authenticateJwtController.handle)
 
 const createCompliementController = new CreateCompliementController()
 router.post("/compliemnt", createCompliementController.handle)
+// Put
+// Deletes
 
 export { router };
